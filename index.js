452eed96729c78af8497bbaabf81cb534f9a6c15
robotgenis <robotgenis@gmail.com>
2019-01-29 16:50:02 -0500

    0.0.2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<!DOCTYPE html><html><head><title>Page Title</title></head><body><h1>This is a Heading</h1><p>This is a paragraph.</p></body></html>");
+    response.write("<!DOCTYPE html><html><head><title>PowerStackers 5029</title></head><body><h1>Scouting Coming Soon</h1><p>Go powerstackers!.</p></body></html>");
     response.end();
 
 });
