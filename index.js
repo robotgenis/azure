4663fab95fd9fe6fba65d7ead1462b824cb761d9
Brandon Young <robotgenis@gmail.com>
2019-01-31 09:14:43 -0500

    0.1.12

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+//console.log("Server running at http://localhost:%d", port);
 
 
 // var Connection = require('tedious').Connection;
