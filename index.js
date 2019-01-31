5b6cd808278f1ec35ccb4b3f5813851fba3b866f
Brandon Young <robotgenis@gmail.com>
2019-01-30 19:56:53 -0500

    0.1.2

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<!DOCTYPE html><html><head><title>PowerStackers 5029</title></head><body><h1>Scouting Coming Soon</h1><p>Go powerstackers!.</p></body></html>");
-    response.end();
-
+    response.end('<!DOCTYPE html><html><head><title>PowerStackers 5029</title></head><body><h1>Scouting Coming Soon</h1><p>Go powerstackers!.</p></body></html>');
 });
 
 var port = process.env.PORT || 1337;
