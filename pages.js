cd912db0bf3493e7e4a7fc236af81bbb8da57391
Brandon Young <robotgenis@gmail.com>
2019-02-02 09:31:38 -0500

    0.6.0

--- pages.js
+++ pages.js
@@ -1,35 +1,47 @@
 var fs = require('fs');
 
 
-
-exports.pages = {
-    'root' : {'path': '/main.html','url':'/','src': null},
-    'reload' : {'path' : null, 'url': '/reload','src':null},
-    'sql' : {'path' : null, 'url': '/sql','src':null}
-};
+function resetPages(){
+    exports.pages = {
+        'root' : {'path': '/main.html','url':'/','src': null},
+        'reload' : {'path' : null, 'url': '/reload','src':null},
+        'sql' : {'path' : null, 'url': '/sql','src':null}
+    };
+}
 
 function loadPages(){
+    resetPages();
+
     for(name in exports.pages){
         if(exports.pages[name].path != null){
-           loadPage(name);
+            exports.pages[name].path = baseDir + exports.pages[name].path;
+            loadPage(name);
         }
     }
-    var files = fs.readdirSync(baseDir + '/html/');
-    for(i in files){
-        var name = files[i];
-        //console.log(name);
-        exports.pages[name] = {'path' : '/html/' + name, 'url': '/' + name, src: null};
-        loadPage(name);
-    }
-    exports.pages.reload.src = function(request) {loadPages();};
+    loadFolder('/html/');
+    exports.pages.reload.src = function(request) {loadPages();return "<a href='/'>return</a>"};
     exports.pages.sql.src = function(request) {exports.sql.runCommand('SELECT username, team from dbo.users', function(results){console.log(results);})};
 }
 
+function loadFolder(path){
+    var files = fs.readdirSync(baseDir + path);
+    for(i in files){
+        var name = files[i];
+        //console.log(baseDir + path + name);
+        if(name.includes(".")){
+            exports.pages[name] = {'path' : baseDir + path + name, 'url': '/' + name, src: null};
+            loadPage(name);
+        }else{
+            loadFolder(path + name + '/');
+        }
+    }
+}
+
 function loadPage(page){
-    fs.readFile(baseDir + exports.pages[page].path, function(err, data) {
+    fs.readFile(exports.pages[page].path, function(err, data) {
         exports.pages[page].src = function(request) {return data};
     });
 }
 
-
+resetPages();
 exports.reloadPages = function(){loadPages();};
